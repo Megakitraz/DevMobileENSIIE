fd483c5a19c66543dfd1acff0cc0f3141ad86ced
lucasSelve <selve.lucas59@gmail.com>
2024-01-04 12:12:32 +0100

    TP4 Q2 pas fini

--- TodoManonPoulain/app/build.gradle.kts
+++ TodoManonPoulain/app/build.gradle.kts
@@ -86,6 +86,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.foundation:foundation-layout-android:1.5.4")
+    implementation("androidx.leanback:leanback:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/user/UserActivity.kt
@@ -0,0 +1,13 @@
+package com.manonpoulain.todo.user
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.manonpoulain.todo.R
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user2)
+    }
+
+}
