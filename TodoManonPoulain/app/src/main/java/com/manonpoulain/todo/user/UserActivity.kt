25f02670b49ab451a260034778c3660f38832934
lucasSelve <selve.lucas59@gmail.com>
2024-01-08 14:12:24 +0100

    before merge

--- TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/user/UserActivity.kt
+++ TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/user/UserActivity.kt
@@ -7,7 +7,7 @@ import com.manonpoulain.todo.R
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user2)
+        setContentView(R.layout.activity_user)
     }
 
 }
