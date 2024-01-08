e4e5b0ed18574f5982b2313b94cee50246cdabdd
Louan Ourvouai <megakitraz3000@gmail.com>
2024-01-08 15:27:19 +0100

    TP4 Q4 terminé

--- TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/data/UserWebService.kt
+++ TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/data/UserWebService.kt
@@ -1,9 +1,33 @@
 package com.manonpoulain.todo.data
 
+import android.graphics.Bitmap
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import java.io.File
 
 interface UserWebService {
     @GET("/sync/v9/user/")
     suspend fun fetchUser(): Response<User>
+
+    @Multipart
+    @POST("sync/v9/update_avatar")
+    suspend fun updateAvatar(@Part avatar: MultipartBody.Part): Response<User>
+
+
+    private fun Bitmap.toRequestBody(): MultipartBody.Part {
+        val tmpFile = File.createTempFile("avatar", "jpg")
+        tmpFile.outputStream().use { // *use*: open et close automatiquement
+            this.compress(Bitmap.CompressFormat.JPEG, 100, it) // *this* est le bitmap ici
+        }
+        return MultipartBody.Part.createFormData(
+            name = "avatar",
+            filename = "avatar.jpg",
+            body = tmpFile.readBytes().toRequestBody()
+        )
+    }
 }
--- TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/user/UserActivity.kt
+++ TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/user/UserActivity.kt
@@ -3,7 +3,10 @@ package com.manonpoulain.todo.user
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -12,10 +15,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import coil.compose.AsyncImage
-import com.manonpoulain.todo.R
+import com.manonpoulain.todo.data.Api
+import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +32,17 @@ class UserActivity : AppCompatActivity() {
         setContent {
             var bitmap: Bitmap? by remember { mutableStateOf(null) }
             var uri: Uri? by remember { mutableStateOf(null) }
+            val scope = rememberCoroutineScope()
+
+            val takePicture = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+                bitmap = it
+                scope.launch {
+                    Api.userWebService.updateAvatar(bitmap!!.toRequestBody())
+                }
+            }
+
+
+
             Column {
                 AsyncImage(
                     modifier = Modifier.fillMaxHeight(.2f),
@@ -31,7 +50,11 @@ class UserActivity : AppCompatActivity() {
                     contentDescription = null
                 )
                 Button(
-                    onClick = {},
+                    onClick = {
+                        takePicture.launch()
+                        //Envoie la photo
+
+                    },
                     content = { Text("Take picture") }
                 )
                 Button(
@@ -40,7 +63,18 @@ class UserActivity : AppCompatActivity() {
                 )
             }
         }
-        setContentView(R.layout.activity_user)
     }
 
+}
+
+private fun Bitmap.toRequestBody(): MultipartBody.Part {
+    val tmpFile = File.createTempFile("avatar", "jpg")
+    tmpFile.outputStream().use { // *use* se charge de faire open et close
+        this.compress(Bitmap.CompressFormat.JPEG, 100, it) // *this* est le bitmap ici
+    }
+    return MultipartBody.Part.createFormData(
+        name = "avatar",
+        filename = "avatar.jpg",
+        body = tmpFile.readBytes().toRequestBody()
+    )
 }
